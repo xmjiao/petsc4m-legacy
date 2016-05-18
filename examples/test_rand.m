c9735d3f72cd59b67563f07a65839021006f9de3
Xiangmin Jiao <xmjiao@gmail.com>
2016-05-18 16:09:36 -0400

    Added a simple example.

---
+++ examples/test_rand.m
@@ -0,0 +1,15 @@
+% Set up a test matrix
+rng(100);
+A = sprand(100,100,0.3);
+A = A + eye(100, 'like', A);
+[rowptr, colind, val] = crs_matrix(A); % This requires MSPACK
+b = rand(100,1);
+
+% Solve using default KSP solver and preconditioner
+[x,flag,relres,iter] = mptSolveCRS(rowptr, colind, val, b);
+
+% Solve using GMRES with Jacobi preconditioner with default options
+[x,flag,relres,iter] = mptSolveCRS(rowptr, colind, val, b, PETSC_KSPGMRES, 0, int32(0), PETSC_PCJACOBI);
+
+% Solve using SuperLU (assuming SuperLU was installed)
+[x,flag,relres,iter] = mptGMRES_crs(rowptr, colind, val, b, PETSC_KSPPREONLY, 0, int32(0), PETSC_PCLU, PETSC_MATSOLVERSUPERLU);
