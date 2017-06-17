bbf4deada72eaaea12a4eaf2b6ef7767ba86d4f3
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-17 12:37:06 -0400

    Changed test script

--- tests/test_petsc.m
+++ tests/test_petsc.m
@@ -1,18 +1,20 @@
 function test_petsc
 % test script for petsc4m
 
-% Test vectors
-mtest petscVecToArray
-mtest petscVecCreateFromArray
-mtest petscVecAXPY
-mtest petscVecAYPX
 
-% Test matrices
-mtest petscMatAIJToCRS.m
-mtest petscMatCreateAIJFromCRS
+% Test vectors, matrices, and KSP solvers
+lines = [grep_pattern([petscroot '/Vec/petscVec*.m'], '%\!test'), ...
+    grep_pattern([petscroot '/Mat/petsc*.m'], '%\!test'), ...
+    grep_pattern([petscroot '/KSP/petsc*.m'], '%\!test'), ...
+    grep_pattern([petscroot '/CRS/petsc*.m'], '%\!test'), ...
+    grep_pattern([petscroot '/petsc*.m'], '%\!test')];
 
-% Test solvers
-mtest petscSolveCRS
-mtest petscSolve
+files = regexp(lines, '[\.\/\\\w]+.m', 'match');
+
+for j=1:length(files)
+    file = files{j};
+    fprintf(1, 'Testing file %s: ', file(length(petscroot)+2:end));
+    mtest(file);
+end
 
 end
