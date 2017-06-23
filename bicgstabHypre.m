0c3578d417a72157f2ace9b0567058f1051131d4
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-23 16:57:41 -0400

    Added bicgstabHypre and bicgstabPetsc

--- bicgstabHypre.m
+++ bicgstabHypre.m
@@ -1,33 +1,28 @@
-function varargout = gmresHypre(varargin)
-% Solves a sparse system using GMRES with BoomerAMG as right-preconditioning
+function varargout = bicgstabHypre(varargin)
+% Solves a sparse system using BiCGStab with BoomerAMG as right-preconditioning
 %
 % Syntax:
-%    x = gmresHypre(A, b) solves a sparse linear system for a matrix in
-%    MATLAB's built-in sparse format using PETSc's GMRES solver with 
+%    x = bicgstabHypre(A, b) solves a sparse linear system for a matrix in
+%    MATLAB's built-in sparse format using PETSc's BiCGStab solver with 
 %    Hypre's BoomerAMG as the right preconditioner.
 %    By default, HMIS coarsening and FF1 interpolation are used with Hypre. 
 %
-%    x = gmresHypre(rowptr, colind, vals, b) takes a matrix in the CRS
+%    x = bicgstabHypre(rowptr, colind, vals, b) takes a matrix in the CRS
 %    format instead of MATLAB's built-in sparse format.
 %
-%    x = gmresHypre(A, b, restart)
-%    x = gmresHypre(rowptr, colind, vals, b, restart)
-%    allows you to specify the restart parameter for GMRES. The default
-%    value is 30. You can preserve the default value by passing [].
-%
-%    x = gmresHypre(A, b, restart, rtol, maxiter)
-%    x = gmresHypre(rowptr, colind, vals, b, restart, rtol, maxiter) 
+%    x = bicgstabHypre(A, b, rtol, maxiter)
+%    x = bicgstabHypre(rowptr, colind, vals, b, rtol, maxiter) 
 %    allows you to specify the relative tolerance and the maximum number 
 %    of iterations. Their default values are 1.e-5 and 10000, respectively. 
 %    Use 0 or [] to preserve default values of rtol and maxiter.
 %
-%    x = gmresHypre(A, b, restart, rtol, maxiter, x0)
-%    x = gmresHypre(rowptr, colind, vals, b, restart, rtol, maxiter, x0) 
+%    x = bicgstabHypre(A, b, rtol, maxiter, x0)
+%    x = bicgstabHypre(rowptr, colind, vals, b, rtol, maxiter, x0) 
 %    takes an initial solution in x0. Use 0 or [] to preserve the default 
 %    initial solution (all zeros).
 %
-%    x = gmresHypre(A, b, restart, rtol, maxit, x0, coarsen, interp)
-%    x = gmresHypre(rowptr, colind, vals, b, restart, rtol, maxit, x0, coarsen, interp)
+%    x = bicgstabHypre(A, b, rtol, maxit, x0, coarsen, interp)
+%    x = bicgstabHypre(rowptr, colind, vals, b, rtol, maxit, x0, coarsen, interp)
 %    allows you to specify different coarsening and interpolation 
 %    strategies for BoomerAMG.
 %
@@ -45,16 +40,16 @@ function varargout = gmresHypre(varargin)
 %    - Others: 'direct', 'multipass', 'multipass-wts', 'ext+i',
 %              'ext+i-cc', 'standard', 'standard-wts', 'FF'
 %
-%    [x, flag, relres, iter, reshis, times] = gmresHypre(...)
+%    [x, flag, relres, iter, reshis, times] = bicgstabHypre(...)
 %    returns the solution vector x, the flag (KSPConvergedReason), relative
 %    residual, number of iterations, history of residual used in convergence
 %    test (typically preconditioned residual), and the execution times in
 %    setup and solve.
 %
-% SEE ALSO: bicgstabHypre, gmresPetsc, petscSolve
+% SEE ALSO: gmresHypre, bicgstabPetsc, petscSolve
 
 if nargin==0
-    help gmresHypre
+    help bicgstabHypre
     return;
 end
 
@@ -75,37 +70,31 @@ else
 end
 
 if nargin >= next_index + 1 && ~isempty(varargin{next_index + 1})
-    opts = [' -ksp_gmres_restart ' int2str(varargin{next_index + 1})];
-else
-    opts = ' -ksp_gmres_restart 30';
-end
-
-if nargin >= next_index + 2 && ~isempty(varargin{next_index + 2})
-    rtol = varargin{next_index + 2};
+    rtol = varargin{next_index + 3};
 else
     rtol = 0;
 end
 
-if nargin >= next_index + 3 && ~isempty(varargin{next_index + 3})
-    maxiter = int32(varargin{next_index + 3});
+if nargin >= next_index + 2 && ~isempty(varargin{next_index + 2})
+    maxiter = int32(varargin{next_index + 2});
 else
     maxiter = int32(0);
 end
 
-if nargin >= next_index + 4 && ~isempty(varargin{next_index + 4})
-    x0 = varargin{next_index + 4};
+if nargin >= next_index + 3 && ~isempty(varargin{next_index + 3})
+    x0 = varargin{next_index + 3};
 else
     x0 = zeros(0, 1);
 end
 
-if nargin >= next_index + 5 && ~isempty(varargin{next_index + 5})
-    opts = [opts ' -pc_hypre_boomeramg_coarsen_type ' varargin{next_index + 5}];
+if nargin >= next_index + 4 && ~isempty(varargin{next_index + 4})
+    opts = ['-pc_hypre_boomeramg_coarsen_type ' varargin{next_index + 4}];
 else
-    opts = [opts ' -pc_hypre_boomeramg_coarsen_type HMIS'];
+    opts = ' -pc_hypre_boomeramg_coarsen_type HMIS';
 end
 
-if nargin >= next_index + 6 && ~isempty(varargin{next_index + 6})
-    opts = [opts ' -pc_hypre_boomeramg_interp_type ' varargin{next_index + 6}];
+if nargin >= next_index + 5 && ~isempty(varargin{next_index + 5})
+    opts = [opts ' -pc_hypre_boomeramg_interp_type ' varargin{next_index + 5}];
 elseif contains(opts, 'MIS')
     opts = [opts ' -pc_hypre_boomeramg_interp_type FF1'];
 else
@@ -113,7 +102,7 @@ else
 end
 
 [varargout{1:nargout}] = petscSolveCRS(Arows, Acols, Avals, ...
-    b, PETSC_KSPGMRES, rtol, maxiter, PETSC_PCHYPRE, 'right', x0, opts);
+    b, PETSC_KSPBCGS, rtol, maxiter, PETSC_PCHYPRE, 'right', x0, opts);
 end
 
 function test %#ok<DEFNU>
@@ -126,7 +115,7 @@ function test %#ok<DEFNU>
 %! b = s.b;
 %! rtol = 1.e-5;
 
-%! [x,flag,relres,iter,reshis,times] = gmresHypre(A, b, [], rtol);
+%! [x,flag,relres,iter,reshis,times] = bicgstabHypre(A, b, [], rtol);
 %! assert(norm(b - A*x) < rtol * norm(b))
 
 end
--- bicgstabPetsc.m
+++ bicgstabPetsc.m
@@ -1,45 +1,40 @@
-function varargout = gmresPetsc(varargin)
-% Solves a sparse system using GMRES and a right-preconditioner
+function varargout = bicgstabPetsc(varargin)
+% Solves a sparse system using BiCGStab and a right-preconditioner
 %
 % Syntax:
-%    x = gmresPetsc(A, b) solves a sparse linear system for a matrix in
-%    MATLAB's built-in sparse format using PETSc's GMRES solver with a
+%    x = bicgstabPetsc(A, b) solves a sparse linear system for a matrix in
+%    MATLAB's built-in sparse format using PETSc's BiCGStab solver with a
 %    right preconditioner. The default preconditioner is ILU0.
 %
-%    x = gmresPetsc(rowptr, colind, vals, b) takes a matrix in the CRS
+%    x = bicgstabPetsc(rowptr, colind, vals, b) takes a matrix in the CRS
 %    format instead of MATLAB's built-in sparse format.
 %
-%    x = gmresPetsc(A, b, restart)
-%    x = gmresPetsc(rowptr, colind, vals, b, restart)
-%    allows you to specify the restart parameter for GMRES. The default
-%    value is 30. You can preserve the default value by passing [].
-%
-%    x = gmresPetsc(A, b, restart, rtol, maxiter)
-%    x = gmresPetsc(rowptr, colind, vals, b, restart, rtol, maxiter) 
+%    x = bicgstabPetsc(A, b, rtol, maxiter)
+%    x = bicgstabPetsc(rowptr, colind, vals, b, rtol, maxiter) 
 %    allows you to specify the relative tolerance and the maximum number 
 %    of iterations. Their default values are 1.e-5 and 10000, respectively. 
 %    Use 0 or [] to preserve default values of rtol and maxiter.
 %
-%    x = gmresPetsc(A, b, restart, rtol, maxiter, x0)
-%    x = gmresPetsc(rowptr, colind, vals, b, restart, rtol, maxiter, x0)
+%    x = bicgstabPetsc(A, b, rtol, maxiter, x0)
+%    x = bicgstabPetsc(rowptr, colind, vals, b, rtol, maxiter, x0)
 %    takes an initial solution in x0. Use 0 or [] to preserve the default 
 %    initial solution (all zeros).
 %
-%    x = gmresPetsc(A, b, restart, rtol, maxit, x0, pctype, opts)
-%    x = gmresPetsc(rowptr, colind, vals, b, restart, rtol, maxit, x0, pctype)
+%    x = bicgstabPetsc(A, b, rtol, maxit, x0, pctype, opts)
+%    x = bicgstabPetsc(rowptr, colind, vals, b, rtol, maxit, x0, pctype)
 %    allows you to specify a preconditioner (such as PETSC_PCHYPRE) and its
 %    options in a string (such as '-pc_hypre_boomeramg_coarsen_type HMIS').
 %    
-%    [x, flag, relres, iter, reshis, times] = gmresPetsc(...)
+%    [x, flag, relres, iter, reshis, times] = bicgstabPetsc(...)
 %    returns the solution vector x, the flag (KSPConvergedReason), relative
 %    residual, number of iterations, history of residual used in convergence
 %    test (typically preconditioned residual), and the execution times in
 %    setup and solve.
 %
-% SEE ALSO: gmresHypre, bicgstabPetsc, petscSolve
+% SEE ALSO: bicgstabHypre, bicgstabPetsc, petscSolve
 
 if nargin==0
-    help gmresPetsc
+    help bicgstabPetsc
     return;
 end
 
@@ -60,41 +55,37 @@ else
 end
 
 if nargin >= next_index + 1 && ~isempty(varargin{next_index + 1})
-    opts = [' -ksp_gmres_restart ' int2str(varargin{next_index + 1})];
-else
-    opts = ' -ksp_gmres_restart 30';
-end
-
-if nargin >= next_index + 2 && ~isempty(varargin{next_index + 2})
-    rtol = varargin{next_index + 2};
+    rtol = varargin{next_index + 1};
 else
     rtol = 0;
 end
 
-if nargin >= next_index + 3 && ~isempty(varargin{next_index + 3})
-    maxiter = int32(varargin{next_index + 3});
+if nargin >= next_index + 2 && ~isempty(varargin{next_index + 2})
+    maxiter = int32(varargin{next_index + 2});
 else
     maxiter = int32(0);
 end
 
-if nargin >= next_index + 4 && ~isempty(varargin{next_index + 4})
-    x0 = varargin{next_index + 4};
+if nargin >= next_index + 3 && ~isempty(varargin{next_index + 3})
+    x0 = varargin{next_index + 3};
 else
     x0 = zeros(0, 1);
 end
 
-if nargin >= next_index + 5 && ~isempty(varargin{next_index + 5})
-    pctype = varargin{next_index + 5};
+if nargin >= next_index + 4 && ~isempty(varargin{next_index + 4})
+    pctype = varargin{next_index + 4};
 else
     pctype = '';
 end
 
-if nargin >= next_index + 6 && ~isempty(varargin{next_index + 6})
-    opts = [opts ' ' varargin{next_index + 6}];
+if nargin >= next_index + 5 && ~isempty(varargin{next_index + 5})
+    opts = varargin{next_index + 5};
+else
+    opts = '';
 end
 
 [varargout{1:nargout}] = petscSolveCRS(Arows, Acols, Avals, ...
-    b, PETSC_KSPGMRES, rtol, maxiter, pctype, 'right', x0, opts);
+    b, PETSC_KSPBCGS, rtol, maxiter, pctype, 'right', x0, opts);
 end
 
 function test %#ok<DEFNU>
@@ -107,7 +98,7 @@ function test %#ok<DEFNU>
 %! b = s.b;
 %! rtol = 1.e-5;
 
-%! [x,flag,relres,iter,reshis,times] = gmresPetsc(A, b, [], rtol);
+%! [x,flag,relres,iter,reshis,times] = bicgstabPetsc(A, b, [], rtol);
 %! assert(norm(b - A*x) < rtol * norm(b))
 
 end
